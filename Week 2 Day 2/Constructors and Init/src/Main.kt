6d2cc18dd3f81fcee5908492d24ca69d1dd88ca9
Krishna Chaitanya Vodnala <vkchaithu07@gmail.com>
2026-05-25 20:13:02 +0530

    Added Week 2 Day 2

---
+++ Week 2 Day 2/Constructors and Init/src/Main.kt
@@ -0,0 +1,67 @@
+// Week 2, Day 2 — Constructors and Init
+class bankAccount(val name: String) {
+    init {
+        println("Welcome $name to XYZ Bank")
+    }
+
+    var age: Int = 0
+    var bal: Int = 0
+
+    constructor(name: String, age: Int): this(name) {
+        this.age = age
+    }
+
+    constructor(name: String, age: Int, bal: Int): this(name) {
+        this.age = age
+        this.bal = bal
+    }
+
+    fun deposit(amnt: Int) {
+        bal += amnt
+        println("Your current balance is $bal")
+    }
+
+    fun Withdraw(amnt: Int) {
+        if(bal - amnt >= 0) {
+            bal -= amnt
+            println("Your current balance is $bal")
+        }else {
+            println("Insufficient funds!")
+        }
+    }
+
+    fun checkBalance() {
+        println("Your current balance is $bal")
+    }
+}
+
+fun main() {
+    val ac1 = bankAccount("Krishna")
+    val ac2 = bankAccount("Mano", 16)
+    val ac3 = bankAccount("Kiran", 44, 1116)
+
+    println()
+
+    // Operations on ac1
+    ac1.deposit(500)
+    ac1.deposit(1000)
+    ac1.Withdraw(300)
+    ac1.checkBalance()
+
+    println()
+
+    // Operations on ac2
+    ac2.deposit(2000)
+    ac2.Withdraw(2500)
+    ac2.Withdraw(500)
+    ac2.checkBalance()
+
+    println()
+
+    // Operations on ac3
+    ac3.checkBalance()
+    ac3.deposit(884)
+    ac3.Withdraw(1000)
+    ac3.Withdraw(2000)
+    ac3.checkBalance()
+}
